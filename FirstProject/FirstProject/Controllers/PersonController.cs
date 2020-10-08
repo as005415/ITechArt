b44d9354890e3455475a30cc98c7696c3552044a
Mark Shestakov <as005424@g.bstu.by>
2020-10-08 16:18:04 +0300

    fix PersonController
    db -> _context; make _context readonly; apply code style

--- FirstProject/FirstProject/Controllers/PersonController.cs
+++ FirstProject/FirstProject/Controllers/PersonController.cs
@@ -11,53 +11,55 @@ namespace FirstProject.Controllers
     [Route("[controller]")]
     public class PersonController : Controller
     {
-        private PersonContext db;
+        private readonly PersonContext _context;
 
         public PersonController(PersonContext context)
         {
-            db = context;
+            _context = context;
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Person>>> GetPersons()
         {
-            return await db.Persons.ToListAsync();
+            return await _context.Persons.ToListAsync();
         }
-        
+
         [HttpGet("{id}")]
         public async Task<ActionResult<Person>> GetPerson(int id)
         {
-            var person = await db.Persons.FindAsync(id);
-            if (person==null)
+            var person = await _context.Persons.FindAsync(id);
+            if (person == null)
             {
                 return NotFound();
             }
+
             return person;
         }
-        
+
         [HttpPost]
-        public async Task<ActionResult<Person>> PostPerson(Person person)
+        public async Task<ActionResult<Person>> AddPerson(Person person)
         {
             if (person == null)
             {
                 return BadRequest();
             }
-            db.Persons.Add(person);
-            await db.SaveChangesAsync();
+
+            _context.Persons.Add(person);
+            await _context.SaveChangesAsync();
             return Ok(person);
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult<Person>> DeletePerson(int id)
         {
-            var person = await db.Persons.FindAsync(id);
+            var person = await _context.Persons.FindAsync(id);
             if (person == null)
             {
                 return NotFound();
             }
 
-            db.Persons.Remove(person);
-            await db.SaveChangesAsync();
+            _context.Persons.Remove(person);
+            await _context.SaveChangesAsync();
             return Ok(person);
         }
     }
